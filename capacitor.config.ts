992648598c5bc15ea56062ff16805847af9902a4
osvaldx <romanonahuel24@gmail.com>
2025-09-10 18:01:33 -0300

    [>] Inicio de proyecto

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.kadoapp.arg',
+  appName: 'KadoAPP',
+  webDir: 'dist'
+};
+
+export default config;
