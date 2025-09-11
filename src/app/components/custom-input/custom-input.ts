aa9392b8b5a987f50cd94425c19fa544abcd27cc
osvaldx <romanonahuel24@gmail.com>
2025-09-11 01:24:36 -0300

    [+] se definen atributos que recibe del padre

--- src/app/components/custom-input/custom-input.ts
+++ src/app/components/custom-input/custom-input.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-custom-input',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './custom-input.html',
   styleUrl: './custom-input.css'
 })
 export class CustomInput {
-
+  
+  @Input() controller!: FormControl;
+  @Input() type?: string;
+  @Input() autocomplete?: string;
+  @Input() placeholder?: string;
+  
 }
