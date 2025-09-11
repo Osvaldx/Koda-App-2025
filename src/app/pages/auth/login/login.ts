e2e781c943038f72379d3c78f81fb2fa4040d232
osvaldx <romanonahuel24@gmail.com>
2025-09-11 01:25:51 -0300

    [+] se consumio el servicio Auth para el login

--- src/app/pages/auth/login/login.ts
+++ src/app/pages/auth/login/login.ts
@@ -1,11 +1,52 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, inject } from '@angular/core';
+import { FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CustomInput } from '../../../components/custom-input/custom-input';
+import { Auth } from '../../../services/auth';
+import { Router } from '@angular/router';
+import { ToastManager } from '../../../services/toast-manager';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule, CustomInput],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
+
 export class Login {
 
+  authService = inject(Auth);
+  router = inject(Router);
+  toastManager = inject(ToastManager);
+
+  public loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required])
+  })
+
+  constructor(private cdr: ChangeDetectorRef) { }
+
+  public async login() {
+    const email = this.loginForm.controls.email.value;
+    const password = this.loginForm.controls.password.value;
+
+    const { error } = await this.authService.signIn(email!, password!);
+
+    if(error) {
+      switch(error.code) {
+        case "invalid_credentials":
+          this.toastManager.show("error", "Credenciales Invalidas", 3000);
+          break;
+        case "user_not_found":
+          this.toastManager.show("error", "No existe un usuario con ese correo", 3000);
+          break;
+        default:
+          this.toastManager.show("error", "Algo ocurrio! Intente más tarde", 3000);
+          break;
+      }
+      return
+    }
+
+    this.toastManager.show("success", "Inicio de Sesion exitoso!", 3000);
+    this.router.navigateByUrl('home', { replaceUrl: true });
+  }
 }
