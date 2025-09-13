439925964d01aa28a686e4f00d400fe76bd2a31a
osvaldx <romanonahuel24@gmail.com>
2025-09-12 21:59:27 -0300

    [+] se registro el splash animado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Login } from './pages/auth/login/login';
 import { Home } from './pages/home/home';
+import { Splash } from './pages/splash/splash';
 
 export const routes: Routes = [
-    { path: "", redirectTo: "login", pathMatch: "full" },
+    { path: "", redirectTo: "splash", pathMatch: "full" },
     { path: "login", component: Login },
-    { path: "home", component: Home }
+    { path: "home", component: Home },
+    { path: "splash", component: Splash }
 ];
