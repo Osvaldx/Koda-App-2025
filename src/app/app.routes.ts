d5e83544c72bba211ca4a765d8851082dbb853e7
osvaldx <romanonahuel24@gmail.com>
2025-09-17 08:43:00 -0300

    [+] se agregaron las rutas del menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,19 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     { path: "", redirectTo: "splash", pathMatch: "full" },
     {
-        path: "auth", children: [
+        path: "auth",
+        children: [
             { path: "login", loadComponent: () => import('./pages/auth/login/login').then(m => m.Login) },
             { path: "register", loadComponent: () => import('./pages/auth/register/register').then(m => m.Register) }
         ]
     },
     { path: "home", loadComponent: () => import('./pages/home/home').then(m => m.Home) },
     { path: "splash", loadComponent: () => import('./pages/splash/splash').then(m => m.Splash) },
+    {
+        path: "menu",
+        children: [
+            { path: "foods", loadComponent: () => import('./pages/menu/foods/foods').then(m => m.Foods) },
+            { path: "drinks", loadComponent: () => import('./pages/menu/drinks/drinks').then(m => m.Drinks) }
+        ]
+    }
 ];
