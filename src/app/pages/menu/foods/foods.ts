046bb46ef8dab54f25ac6135f66911b1096724b4
osvaldx <romanonahuel24@gmail.com>
2025-09-17 08:52:21 -0300

    [+] esta logica es temporal hasta modularizar

--- src/app/pages/menu/foods/foods.ts
+++ src/app/pages/menu/foods/foods.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ToastManager } from '../../../services/toast-manager';
+import { Auth } from '../../../services/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-foods',
@@ -7,5 +10,35 @@ import { Component } from '@angular/core';
   styleUrl: './foods.css'
 })
 export class Foods {
+
+  toast = inject(ToastManager);
+  authService = inject(Auth);
+  router = inject(Router);
+
+  public menuOpen: boolean = false;
+  public profileOpen: boolean = false;
+  public userEmail: string | null = null;
+
+  constructor() {
+    this.authService.user$.subscribe((user) => {
+      this.userEmail = user?.email ?? null;
+    })
+  }
+
+  public async sessionClose() {
+    this.authService.signOut().then( async(resp) => {
+      if(resp.error) {
+        this.toast.show("error", "Ocurrio, un error al cerrar sesión", 3000);
+        console.log(resp.error);
+      } else {
+        this.toast.show("success", "Se cerro la sesión correctamente", 3000);
+        this.router.navigateByUrl('/auth/login');
+      }
+    })
+  }
+
+  public toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
 
 }
