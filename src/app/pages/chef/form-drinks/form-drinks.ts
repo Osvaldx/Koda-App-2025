21862f1eec2b2cc597c653a869090ccc269cbc83
StefaniaAyelen <ayelenbianchi55@gmail.com>
2025-09-18 18:10:56 -0300

    Nuevo form de comidas

---
+++ src/app/pages/chef/form-drinks/form-drinks.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-drinks',
+  imports: [],
+  templateUrl: './form-drinks.html',
+  styleUrl: './form-drinks.css'
+})
+export class FormDrinks {
+
+}
---
+++ src/app/pages/chef/form-foods/form-foods.ts
@@ -0,0 +1,51 @@
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ImageUploadComponent, ImageUploadData } from '../../../components/attachments/picture/picture';
+import { CustomInputChef } from '../../../components/custom-input-chef/custom-input-chef';
+import { ToastManager } from '../../../services/toast-manager';
+
+@Component({
+  selector: 'app-form-foods',
+  imports: [ReactiveFormsModule, CustomInputChef, ImageUploadComponent],
+  templateUrl: './form-foods.html',
+  styleUrl: './form-foods.css'
+})
+export class FormFoods {
+  toast = inject(ToastManager)
+
+  public dishForm = new FormGroup({
+        name: new FormControl('', [Validators.required]),
+        description: new FormControl('', [Validators.required]),
+        time: new FormControl<number | null>(null, [Validators.required]),
+        price: new FormControl<number | null>(null, [Validators.required]),
+    })
+
+  imageData: ImageUploadData | null = null
+  
+  protected onImageDataChange(data: ImageUploadData | null) {
+    this.imageData = data;
+  }
+  
+  hasNotSpaces(){
+    return (this.dishForm.controls.name.value?.trim() == '' || 
+    this.dishForm.controls.description.value?.trim() == '' || 
+    this.dishForm.controls.time.value?.toString().trim() == '' || 
+    this.dishForm.controls.price.value?.toString().trim() == '' )
+  }
+  
+  registerDish(){
+    if (this.hasNotSpaces()) {
+      this.toast.show("error", "Complete todos los campos", 3000);
+      return
+    }
+    if (this.imageData === null) {
+      this.toast.show("error", "Ponga las imagenes", 3000);
+      return
+    }
+
+    return this.toast.show('success', 'Plato cargado con exito!', 3000)
+  }
+
+
+}
+
