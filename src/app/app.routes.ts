43149fcdedbc8e69ca2fedf57786090438473d73
Federico <fededeniard@gmail.com>
2025-09-19 20:50:48 -0300

    Feature: Implementé el login anonimo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,23 +1,42 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    { path: "", redirectTo: "splash", pathMatch: "full" },
-    {
-        path: "auth",
-        children: [
-            { path: "login", loadComponent: () => import('./pages/auth/login/login').then(m => m.Login) },
-            { path: "register", loadComponent: () => import('./pages/auth/register/register').then(m => m.Register) }
-        ]
-    },
-    { path: "home", loadComponent: () => import('./pages/home/home').then(m => m.Home) },
-    { path: "splash", loadComponent: () => import('./pages/splash/splash').then(m => m.Splash) },
-    { path: "maitre", loadComponent: () => import('./pages/maitre/maitre').then(m => m.Maitre) },
-    {
-        path: "menu",
-        children: [
-            { path: "foods", loadComponent: () => import('./pages/menu/foods/foods').then(m => m.Foods) },
-            { path: "drinks", loadComponent: () => import('./pages/menu/drinks/drinks').then(m => m.Drinks) }
-        ]
-    },
-    {path: "waiterscreen", loadComponent: () => import('./pages/waiter-screen/waiter-screen').then(m => m.WaiterScreen)}
+  { path: '', redirectTo: 'splash', pathMatch: 'full' },
+  {
+    path: 'auth',
+    children: [
+      {
+        path: 'login',
+        loadComponent: () => import('./pages/auth/login/login').then((m) => m.Login),
+      },
+      {
+        path: 'register',
+        loadComponent: () => import('./pages/auth/register/register').then((m) => m.Register),
+      },
+      {
+        path: 'guest',
+        loadComponent: () => import('./pages/auth/register/guest/guest').then((m) => m.Guest),
+      },
+    ],
+  },
+  { path: 'home', loadComponent: () => import('./pages/home/home').then((m) => m.Home) },
+  { path: 'splash', loadComponent: () => import('./pages/splash/splash').then((m) => m.Splash) },
+  { path: 'maitre', loadComponent: () => import('./pages/maitre/maitre').then((m) => m.Maitre) },
+  {
+    path: 'menu',
+    children: [
+      {
+        path: 'foods',
+        loadComponent: () => import('./pages/menu/foods/foods').then((m) => m.Foods),
+      },
+      {
+        path: 'drinks',
+        loadComponent: () => import('./pages/menu/drinks/drinks').then((m) => m.Drinks),
+      },
+    ],
+  },
+  {
+    path: 'waiterscreen',
+    loadComponent: () => import('./pages/waiter-screen/waiter-screen').then((m) => m.WaiterScreen),
+  },
 ];
